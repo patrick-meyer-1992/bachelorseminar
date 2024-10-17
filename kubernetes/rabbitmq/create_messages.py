f36ac29c1d4eef129bb96f256712fe64051c4860
Patrick Meyer <patrick.meyer1992@gmail.com>
2024-10-17 19:29:39 +0200

    Initial rabbitmq

---
+++ kubernetes/rabbitmq/create_messages.py
@@ -0,0 +1,38 @@
+import pika
+import json
+
+# Connection parameters
+rabbitmq_host = 'localhost'
+rabbitmq_port = 5672
+rabbitmq_user = 'guest'
+rabbitmq_password = 'guest'
+
+# Queue and exchange parameters
+queue_name = 'json_queue'
+exchange_name = 'json_exchange'
+routing_key = 'json_key'
+
+# Connect to RabbitMQ
+credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_password)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, port=rabbitmq_port, credentials=credentials))
+channel = connection.channel()
+
+# Declare exchange
+channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
+
+# Declare queue
+channel.queue_declare(queue=queue_name, durable=True)
+
+# Bind queue to exchange
+channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=routing_key)
+
+# Example of sending a JSON message
+with open("D:/GitHub/bachelorseminar/simulation/workspaces.json") as f:
+    message = f.read()
+
+channel.basic_publish(exchange=exchange_name, routing_key=routing_key, body=message)
+
+print(f"Sent message: {message}")
+
+# Close connection
+connection.close()
